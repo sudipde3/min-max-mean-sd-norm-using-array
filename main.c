014da716a95d43614d9f5c22b4ec016d95fcbcd4
Sudip De <138770445+sudipde3@users.noreply.github.com>
2023-09-07 02:39:45 +0530

    main.c

--- main.c
+++ main.c
@@ -1,15 +1,4 @@
-// Name: Sudip De
-// Entry No: 2023MAS7152
-
-/*     ------     Programming Assignment-1     ------     */
-
-/*--- Write a C program which should create 6 different arrays (using different 
-math functions) of size N1, N2, N3, N4, N5, N6 obtained from user input. 
-(Note, Ni (i = 1, 2,3,4,5, 6) should be different values)
--This program should find min, max, mean, SD values and pth norm 
-(where p value should be entered by user) of all the arrays.
--Moreover, print which array has minimum value of pth norm.
--Next, print values of each array which lies in the interval [mean-sd, mean+sd] ---*/
+//github: sudipde3
 
 #include <stdio.h>
 #include <math.h>
